738885f62f33fb2733826cb5db64ad7720e56d55
Mufaddal Hamid <mufaddalhamidofficial@gmail.com>
2024-01-12 18:01:44 +0530

    first commit

---
+++ helper.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def plot_loss_curves(history: tf.keras.callbacks.History):
+    """
+    Returns separate loss curves for training and validation metrics.
+
+    Args:
+        history: A TensorFlow History object.
+
+    Returns:
+        Plots of training/validation loss and accuracy metrics.
+
+    """
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+
+    accuracy = history.history["accuracy"]
+    val_accuracy = history.history["val_accuracy"]
+
+    epochs = range(len(history.history["loss"]))
+
+    # Plot loss
+    plt.plot(epochs, loss, label="training_loss")
+    plt.plot(epochs, val_loss, label="val_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot accuracy
+    plt.figure()
+    plt.plot(epochs, accuracy, label="training_accuracy")
+    plt.plot(epochs, val_accuracy, label="val_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
